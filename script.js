6243dd68047d1e4d0ddbd65ccf01e4535d84b02a
Adrian Krebs <info@tie-international.com>
2024-07-01 11:04:58 +0200

    Initial commit

--- script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const button = document.createElement('button');
-    button.textContent = 'I want to go home';
-    button.id = 'homeButton';
-    button.onclick = function() {
-        alert('Going home!');
-    };
-    document.getElementById('container').appendChild(button);
-});
